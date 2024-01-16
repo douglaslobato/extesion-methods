887c54f7114ada4bca0d2329194f357df7d374c4
douglaslobato <douglas.dbko@hotmail.com>
2024-01-16 17:46:14 -0300

    commit inicial

---
+++ MetodosExtensao/ObjectExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosExtensao
+{
+    public static class ObjectExtensions
+    {
+        public static int ToIntZeroAble(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return 0;
+
+            int intValue;
+            if (int.TryParse(value, out intValue))
+                return intValue;
+
+            return 0;
+        }
+
+        public static string ToStringOrEmpty<T>( this T obj)
+        {
+            if (obj == null)
+                return string.Empty;          
+
+            return obj.ToString();
+        }
+    }
+}
---
+++ MetodosExtensao/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MetodosExtensao;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+string strNumber = "123";
+string strNumber2 = "";
+string strNumber3 = null;
+
+int intNumberConverted = strNumber.ToIntZeroAble();
+int intNumberConverted2 = strNumber2.ToIntZeroAble();
+int intNumberConverted3 = strNumber3.ToIntZeroAble();
+
+
+
+Console.WriteLine(intNumberConverted);
+Console.WriteLine(intNumberConverted2);
+Console.WriteLine(intNumberConverted3);
+
+int number1 = 1;
+object a = null;
+
+string converted1 = number1.ToStringOrEmpty();
+string converted2 = a.ToStringOrEmpty();
+
+Console.WriteLine(converted1);
+Console.WriteLine(converted2);
+
+Console.ReadKey();
